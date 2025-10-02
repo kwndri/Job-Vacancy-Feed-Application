26167cc04efcaa70d476f17696bd39e7b664491a
kwndri <kondritsas14@gmail.com>
2025-10-02 14:50:08 +0300

    Login and sign-up routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { JobListComponent } from './components/job-list-component/job-list-compo
 import { JobDescriptionComponenent } from './components/job-description-componenent/job-description-componenent';
 import { NotFoundComponent } from './components/not-found-component/not-found-component';
 import { JobApplyComponent } from './components/job-apply-component/job-apply-component';
+import { LoginPageComponent } from './components/login-page-component/login-page-component';
+import { SignUpPageComponent } from './components/sign-up-page-component/sign-up-page-component';
 
 export const routes: Routes = [
   {
@@ -21,6 +23,14 @@ export const routes: Routes = [
   {
     path: 'jobs/:jobId/apply',
     component: JobApplyComponent,
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+  },
+  {
+    path: 'signup',
+    component: SignUpPageComponent,
   },
   {
     path: '**',
