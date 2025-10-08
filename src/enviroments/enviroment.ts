e5f613976561d797381f3996e58979a3ce16e2f1
kwndri <kondritsas14@gmail.com>
2025-10-08 16:58:03 +0300

    Stop tracking environment.ts

--- src/enviroments/enviroment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiToken:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJHZW9yZ2VEQE9ESU4tS29uc3VsdC5ubyIsImtpZCI6IjE3MmU3OWY2LTcxZWUtNDJiNy1hZTc1LTM3OTM0M2JiZWJkZCIsImlzcyI6Im5hdi1ubyIsImF1ZCI6ImZlZWQtYXBpLXYyIiwiaWF0IjoxNzI5MjU1NjMxLCJleHAiOm51bGx9.iWVPjNV0moSrsz4G1N2KEcB24Wiji4hY_HVNEeetdTY', //Add here your API token from NAV,
-};
